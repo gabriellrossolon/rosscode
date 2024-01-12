1954e6015937f36d15ada80d305ecfb839fa7c81
Gabriell Rossolon <gabrirossolon@yahoo.com.br>
2024-01-12 15:13:46 -0400

    animações data-aos + keyframe

--- script.js
+++ script.js
@@ -14,10 +14,11 @@ function changeIcon() {
     if (menu.classList.contains("fa-bars")){
         menu.classList.remove("fa-bars") || menu.classList.add("fa-bars-staggered")
     } else {
-        menu.classList.remove("fa-bars-staggered") || menu.classList.add("fa-bars")
-           
+        menu.classList.remove("fa-bars-staggered") || menu.classList.add("fa-bars") 
     } 
 }  
 
 
 /////
+
+AOS.init();
